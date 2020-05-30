2bfcc8bd6a144ff17e2d16159974ec40dc0dcd67
rabah babaci <4rabah@gmail.com>
2020-05-30 10:29:22 -0700

    create the body validation middleware

--- api/projectsRouter/index.js
+++ api/projectsRouter/index.js
@@ -1,8 +1,22 @@
 const express = require("express");
 
 const { getResources } = require("../dbHelpers");
+const { getUndefinedProps } = require("../../utils");
 
 const router = express.Router();
+
+router.post("/resources", async (req, res, next) => {
+  try {
+    const [addedResourceId] = await addResource(req.body);
+  } catch ({ errno, code, message }) {
+    next({
+      message: "The resource could not be retrieved at this moment.",
+      errno,
+      code,
+      reason: message,
+    });
+  }
+});
 
 router.get("/resources", async (req, res, next) => {
   try {
@@ -11,12 +25,34 @@ router.get("/resources", async (req, res, next) => {
     res.status(200).json(resources);
   } catch ({ errno, code, message }) {
     next({
-      message: "The recipes could not be retrieved at this moment.",
+      message: "The resources could not be retrieved at this moment.",
       errno,
       code,
       reason: message,
     });
   }
 });
+
+/*********************  Validation Middleware ********************/
+function validateBody(tableName) {
+  return (req, res, next) => {
+    const { name, description } = req.body;
+
+    const results =
+      tableName === "resources"
+        ? getUndefinedProps({ name, description })
+        : getUndefinedProps({ name, quantity });
+
+    if (!results) {
+      next();
+    } else {
+      res.status(400).json({
+        message: `👉🏼 [ ${results.join(
+          " | "
+        )} ] 👈🏼 missing or incorrectly defined in the request body.`,
+      });
+    }
+  };
+}
 
 module.exports = router;

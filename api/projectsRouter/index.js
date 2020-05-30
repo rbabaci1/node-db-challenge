cbbfde05c94fc1dd57e49f7fe721ea547f28c5e2
rabah babaci <4rabah@gmail.com>
2020-05-30 10:21:52 -0700

    create the get resources enpoint

--- api/projectsRouter/index.js
+++ api/projectsRouter/index.js
@@ -1,5 +1,22 @@
 const express = require("express");
 
+const { getResources } = require("../dbHelpers");
+
 const router = express.Router();
+
+router.get("/resources", async (req, res, next) => {
+  try {
+    const resources = await getResources();
+
+    res.status(200).json(resources);
+  } catch ({ errno, code, message }) {
+    next({
+      message: "The recipes could not be retrieved at this moment.",
+      errno,
+      code,
+      reason: message,
+    });
+  }
+});
 
 module.exports = router;
